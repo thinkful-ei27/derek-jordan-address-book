e86a928f3e95a023077a958d94102aad278cb36d
Glitch (react-address-book-starting-point) <none>
2019-01-22 08:59:57 -0800

    created contacts

--- src/components/address-book.js
+++ src/components/address-book.js
@@ -3,3 +3,34 @@ import React from 'react';
 import Contact from './contact';
 
 // The AddressBook component goes here.  It should be the default export.
+export default class AddressBook extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+        contacts: [{
+            name: 'Alice Aardvark',
+            photo: 'https://api.adorable.io/avatars/64/alice%40thinkful.com',
+            address: '1600 Pennsylvania Ave'
+        }, {
+            name: 'Bob Bear',
+            photo: 'https://api.adorable.io/avatars/64/bob%40thinkful.com',
+            address: '725 5th Ave'
+        }, {
+            name: 'Carol Coyote',
+            photo: 'https://api.adorable.io/avatars/64/carol%40thinkful.com',
+            address: '4 Pennsylvania Plaza'
+        }]
+    
+    };
+  }
+  
+  render () {
+    const contacts = this.state.contacts.map(contact => {
+      return <Contact name={contact.name} address={contact.address} photo={contact.photo} />
+    });
+    return (
+      <div className="addressBook">{contacts}</div>
+    );
+  }
+}
--- src/components/contact.js
+++ src/components/contact.js
@@ -3,3 +3,13 @@ import React from 'react';
 import './contact.css';
 
 // The Contact component goes here.  It should be the default export.
+
+export default function Contact(props) {
+  return (
+    <div className="contact">
+      <img className="contact-photo" src={props.photo} />
+      <p className="contact-name">{props.name}</p>
+      <p className="contact-address">{props.address}</p>
+    </div>
+  );
+}
